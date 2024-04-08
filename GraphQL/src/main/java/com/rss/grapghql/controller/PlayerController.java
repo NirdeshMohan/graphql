e0651ecd3d046b90abf5dd4c0541c204e203534d
Nirdesh Mohan <NirdeshM@Gmail.com>
2024-04-07 21:31:55 -0400

    third commit
    thrd commit

--- GraphQL/src/main/java/com/rss/grapghql/controller/PlayerController.java
+++ GraphQL/src/main/java/com/rss/grapghql/controller/PlayerController.java
@@ -1,8 +1,10 @@
 package com.rss.grapghql.controller;
 
 import com.rss.grapghql.model.Player;
+import com.rss.grapghql.model.Team;
 import com.rss.grapghql.service.PlayerService;
 import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.MutationMapping;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Controller;
 
@@ -25,5 +27,20 @@ public class PlayerController {
     @QueryMapping
     public Optional <Player> findOne(@Argument Integer id){
         return service.findOne(id);
+    }
+
+    @MutationMapping
+    public Player create(@Argument String name, @Argument Team team){
+        return service.create(name, team);
+    }
+
+    @MutationMapping
+    public Player update(@Argument Integer id,@Argument String name, @Argument Team team){
+        return service.update(id,name, team);
+    }
+
+    @MutationMapping
+    public Player delete(@Argument Integer id){
+        return service.delete(id);
     }
 }
